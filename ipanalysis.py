3bab1a76c2da92535d447183b5486bc417b4e33d
Joshu Fisher <joshu.fisher@gmail.com>
2022-07-01 00:24:42 -0700

    Update ipanalysis.py
    streamlit secrets setup

--- ipanalysis.py
+++ ipanalysis.py
@@ -7,7 +7,18 @@ import plotly.graph_objects as go
 st.set_page_config(layout='wide')
 
 # setup credentials
-
+credentials = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+}
 sheetKey = '1f3gA4sIxLqBYQw7bk1igGGNaFtW7LHtO_7BCCmMS1IQ'
 dbSheet1 = 'Survey Data'
 dbSheet2 = 'Org Data'
